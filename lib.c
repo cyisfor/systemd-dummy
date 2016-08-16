4931fd33553ede93bc549ef511faab2229791101
user <nobody>
2016-08-16 04:36:41 +0000

    auto (makemake.pl) 496 4611

--- lib.c
+++ lib.c
@@ -1,7 +1,6 @@
 #include <stdlib.h> // unsetenv
 #include <stdio.h>
 
-
 #define STUB(name) int name() { fputs("systemd: " #name "\n",stderr);
 #define END_STUB return 0; }
 
@@ -11,4 +10,11 @@ END_STUB
 
 STUB(sd_notifyf)
 	unsetenv("NOTIFY_SOCKET");
+END_STUB
+	
+int sd_booted(void) {
+	return 0;
+}
+
+STUB(sd_journal_send_with_location)
 END_STUB
--- make.c
+++ make.c
@@ -9,13 +9,38 @@
 #include <errno.h>
 #include <sys/wait.h> // waitpid
 
-#define soname "libsystemd.so"
-#define longname soname ".0.0"
+#define soname(name) "lib" name ".so"
+#define longname1(name) soname(name) ".0"
+#define longname(name) soname(name) ".0.0"
 
-void build_lib(void) {
+struct path {
+	int dirfd;
+	const char* name;
+};
+
+int mystat(struct path p, struct stat* buf) {
+	if(p.dirfd != AT_FDCWD) {
+		return fstatat(p.dirfd, p.name, buf);
+	}
+	return stat(p.name,buf);
+}
+
+bool need_update(struct path target, struct path dep) {
+	struct stat tstat, dstat;
+	if(0 != mystat(target, &tstat)) {
+		return true;
+	}
+	if(0 != mystat(dep, &dstat)) {
+		error(23,errno,"Couldn't stat a dependency!");
+	}
+	if(dstat.st_mtime > tstat.st_mtime) return true;
+	return false;
+}
+
+void build_lib(struct path lib, struct path source) {
 	const char* cc = getenv("CC");
 	if(cc == NULL) cc = "cc";
-	puts("BUILD " soname);
+	puts("BUILD " lib.name);
 	int pid = fork();
 	if(pid == 0) {
 		execlp(cc,cc,
@@ -25,8 +50,9 @@ void build_lib(void) {
 					 "-Wl,-soname," longname,
 					 "-fpic",
 					 "-o",
-					 soname,
-					 "lib.c");
+					 lib.name,
+					 source.name,
+					 NULL);
 		error(23,errno,"wat");
 	}
 	int status;
@@ -35,40 +61,82 @@ void build_lib(void) {
 		exit(status);
 }
 
-void build_stuff(void) {
+void build_thingy(struct path lib, struct path source) {
 	if(getenv("clean")) {
 		unlink(soname) || puts("UNLINK " soname);
 		return;
 	}
 
-	struct stat target, dep;
-	if(0==stat(soname,&target)) {
-		assert(0==stat("lib.c",&dep));
-		if(dep.st_mtime > target.st_mtime) {
-			build_lib();
-		}
-	} else {
-		build_lib();
+	if(need_update(lib,source,NULL)) {
+			build_lib(lib,source);
 	}
+	struct stat derp;
+	assert(0 == mystat(lib,&derp));
 }
 
-void install(int src, const char* name) {
-	int dest = open("temp",O_WRONLY|O_CREAT|O_TRUNC,0755);
+struct path path(const char* name) {
+	path ret = { 0, name };
+	return ret;
+}
+
+void build_stuff(void) {
+	build_thingy(path(longname("systemd")),path("lib.c"));
+	build_thingy(path(longname("gudev-1.0")),path("gudev.c"));
+}
+
+void install(struct path target, struct path dep) {
+	int source;
+	if(dep.dirfd != AT_FDCWD) {
+		source = openat(dep.dirfd, dep.name, O_RDONLY);
+	} else {
+		source = open(dep.name, O_RDONLY);
+	}
+	if(source<0)
+		error(3,errno,"couldn't open source dep");
+	
+	int dest;
+	if(target.dirfd != AT_FDCWD) {
+		dest = openat(target.dirfd, "temp",O_WRONLY|O_CREAT|O_TRUNC,0755);
+	} else {
+		dest = open("temp",O_WRONLY|O_CREAT|O_TRUNC,0755);
+	}
 	if(dest<0)
 		error(3,errno,"couldn't open temporary file");
+
 	for(;;) {
-		ssize_t amt = sendfile(dest,src,NULL,0x7ffff000);
+		ssize_t amt = sendfile(dest,source,NULL,0x7ffff000);
 		if(amt == 0) break;
 		if(amt < 0) error(2,errno,"sendfile fail");
 	}
-	close(src);
-	if(0!=rename("temp",name))
-		error(4,errno,"couldn't rename temporary file to %s",name);;
+	close(source);
+	int res;
+	if(target.dirfd != AT_FDCWD) {
+		res = renameat(target.dirfd,"temp",target.dirfd,target.name);
+	} else {
+		res = rename("temp",name);
+	}
+	if(res != 0) {
+		error(res,errno,"couldn't rename temporary file to %s",target.name);
+	}
+
 	close(dest);
 }
 
 int main(void)
 {
+	if(getenv("clean")) {
+#define CLEANONE(name)																							\
+		unlink(longname(name)) || puts("UNLINK " longname(name));				\
+		unlink(longname1(name)) || puts("UNLINK " longname1(name)); \
+		unlink(soname(name)) || puts("UNLINK " soname(name));
+		CLEANONE("systemd");
+		CLEANONE("gudev-1.0");
+		chdir(prefix);
+		CLEANONE("systemd");
+		CLEANONE("gudev-1.0");
+		return 0;
+	}
+
 	// drop privs to build if needed
 	if(getuid()==0) {
 		int builder = fork();
@@ -88,51 +156,37 @@ int main(void)
 		waitpid(builder,NULL,0);
 	} else {
 		build_stuff();
-		if(!getenv("clean"))
-			puts("run this as root to install: prefix=/usr/lib ./make");
+		puts("run this as root to install: prefix=/usr/lib ./make");
 		return 0;
 	}
+
 	// we're still root
 	puts("installing");
 	const char* prefix = getenv("prefix");
 	if(prefix==NULL)
 		prefix = "/usr/lib/";
 
-	if(getenv("clean")) {
-		chdir(prefix);
-		unlink(longname) || puts("UNLINK " longname);
-		unlink(soname ".0") || puts("UNLINK " soname ".0");
-		unlink(soname) || puts("UNLINK " soname);
-		return 0;
+	int dest = open(prefix,O_RDONLY | O_SEARCH);
+	assert(dest >= 0);
+
+#define FUCKIT(NAME)																			\
+	struct path target = { AT_FDCWD, longname(NAME) };			\
+	struct path source = { AT_FDCWD, NAME ".c" };						\
+	build_thingy(target,source);														\
+	target.dirfd = dest;																		\
+	source.name = longname(NAME);														\
+	if(need_update1(target,source)) {												\
+		install(target,source);																\
+	}																												\
+	if(fstatat(target.dirfd,longname1(NAME), &derp) != 0) {	\
+		symlinkat(target.name, target.dirfd, longname1(NAME))	\
+			|| puts("SYMLINK" longname1(NAME));								\
+		symlinkat(target.name, target.dirfd, soname(NAME))				\
+			|| puts("SYMLINK" soname(NAME));										\
 	}
 
-	struct stat target, dep;
-	if(0!=stat(soname,&dep)) {
-		error(1,0,"couldn't build " soname);
-	}
+	FUCKIT("systemd");
+	FUCKIT("gudev-1.0");
 
-	// it's either open this, or open the directory it's in,
-	// disk read either way
-	int src = open(soname,O_RDONLY);
-	assert(src > -1);
-
-	chdir(prefix);
-
-	if(stat(longname,&target) == 0) {
-		if(dep.st_mtime > target.st_mtime) {
-			puts("INSTALL " longname);
-			install(src, longname);
-		}
-	} else {
-		puts("INSTALL " longname);
-		install(src, longname);
-	}
-
-	if(stat(soname ".0",&target) != 0) {
-		symlink(longname, soname ".0") || puts("SYMLINK " soname ".0");
-	}
-	if(stat(soname,&target) != 0) {
-		symlink(longname, soname) || puts("SYMLINK " soname);;
-	}
 	return 0;
 }
